3b7d844b2d830dd250a625c581182be02bcac136
MJdelrosa <78661115+soric91@users.noreply.github.com>
2023-06-23 09:23:54 -0500

    Update LecturaPAC.py

--- LecturaPAC.py
+++ LecturaPAC.py
@@ -1,6 +1,7 @@
 from libreriaPAC import *
 import time
 import serial.tools.list_ports
+import math
 
 
 puertos_pac=list(serial.tools.list_ports.comports())
@@ -22,38 +23,20 @@ BYTE_SIZE              =   8
 STOP_BIT               =   1
 TIMEOUT                =   0.5
 
-#REGISTROS LibreriaPAC3120
-TENSION_L1             =  1
-TENSION_L2		  	   =  3
-TENSION_L3             =  5
-CORRIENTE_L1           =  13
-CORRIENTE_L2           =  15
-CORRIENTE_L3           =  17
-POTENCIA_APARENTE_L1   =  19
-POTENCIA_APARENTE_L2   =  21
-POTENCIA_APARENTE_L3   =  23
-POTENCIA_ACTIVA_L1     =  25
-POTENCIA_ACTIVA_L2     =  27
-POTENCIA_ACTIVA_L3     =  29
-POTENCIA_REACTIVA_L1   =  31
-POTENCIA_REACTIVA_L2   =  33
-POTENCIA_REACTIVA_L3   =  35
-FACTOR_DE_POTENCIA_L1  =  37
-FACTOR_DE_POTENCIA_L2  =  39
-FACTOR_DE_POTENCIA_L3  =  41
-
-
+# asiganacion de direcion de memoria segun los datos a pedir del PAC3120
 POTENCIA_ACTIVA_L1_L2_L3 = (25, 27, 29)
 POTENCIA_APARENTE_L1_L2_L3 = (19, 21, 23)
 POTENCIA_REACTIVA_L1_L2_L3 = (31, 33, 35)
 FACTOR_DE_POTENCIA_L1_L2_L3 = (37, 39, 41)
+POTENCIA_TRIFACICA_V = (1, 3, 5) # L1, L2, L3
+POTENCIA_TRIFACICA_I = (13, 15, 17) # A1, A2, A3
 
 ALL_REGISTER_PAC3120 = (63, 65, 67, 19, 25, 31, 37, 1, 13, 55, 21, 27, 33, 29, 35, 41, 5, 17, 55)
 
 #REGISTROS HUAWEI
 POTENCIA_ACTIVA_HUAWEI = 32080
 leer_pac = []
-tiempo_inicio = time.time()
+# tiempo_inicio = time.time()
 for i in range(3):
     leer_pac.append(PAC3120(SLAVE_ADDRESS_PAC[i], FUNCTION_CODE_READ, PORT[i], MODE, VELOCITY, BYTE_SIZE, STOP_BIT, TIMEOUT))
 
@@ -85,30 +68,18 @@ while True:
         print('Potencia Aparente de L1, L2, L3')
         leer_pac[j].read_list(POTENCIA_APARENTE_L1_L2_L3)
         for i, FP in enumerate(results_pac[:len(POTENCIA_APARENTE_L1_L2_L3)]):
-            print(f'Potencia Aparente de L{i+1}')
+            print(f'Potencia Aparente de L{i+1}')            
         
-        tiempo_final = time.time()
-        Tiempo_Codigo = tiempo_final-tiempo_inicio
-        print(Tiempo_Codigo)                 
-        time.sleep(1)
-
-# leer.find_pac(TENSION_L1)
-# leer.find_pac(TENSION_L2)
-# leer.find_pac(TENSION_L3)
-# leer.find_pac(CORRIENTE_L1)
-# leer.find_pac(CORRIENTE_L2)
-# leer.find_pac(CORRIENTE_L3)
-# leer.find_pac(POTENCIA_APARENTE_L1)
-# leer.find_pac(POTENCIA_APARENTE_L2)
-# leer.find_pac(POTENCIA_APARENTE_L3)
-# leer.find_pac(POTENCIA_ACTIVA_L1)
-# leer.find_pac(POTENCIA_ACTIVA_L2)
-# leer.find_pac(POTENCIA_ACTIVA_L3)
-# leer.find_pac(FACTOR_DE_POTENCIA_L1)
-# leer.find_pac(FACTOR_DE_POTENCIA_L2)
-#leer.find_pac(FACTOR_DE_POTENCIA_L3)
-# print(results)  
-
+        leer_pac[j].read_list(POTENCIA_TRIFACICA_V)
+        for i, PTT in enumerate(results_pac[:(len(POTENCIA_TRIFACICA_V))]):
+            voltaje =+ results_pac[i]
+        
+        leer_pac[j].read_list(POTENCIA_TRIFACICA_I)
+        for i, PTT in enumerate(results_pac[:(len(POTENCIA_TRIFACICA_I))]):
+            amperios =+ results_pac[i]        
+        
+        PotenciaTT = math.sqrt(3) * voltaje/3 * amperios/3
+        print(f'Potencia Trifacica total: {PotenciaTT} W')
 
  
 
